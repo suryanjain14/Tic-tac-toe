bfb5b16cb95e3a9414b4b013e31f7c1fc1f1ab75
suryanjain14 <38618974+suryanjain14@users.noreply.github.com>
2018-06-12 22:36:18 +0530

    Add project files.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,9 @@
+play1=''
+play2=''
+def nameinput():
+    global play1
+    global play2
+    play1 =input('Enter the name of player 1:')
+    play2 =input('Enter the name of player 2:')
+no=0
+def engine(no):
